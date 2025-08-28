db8ec0a180b24d737972fbcbbc7d43d2f15554ce
richardccf <richard@carboncopies.org>
2025-08-28 10:48:07 -0500

    updated wouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Base } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,10 +8,12 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <Base basePath="/cnwk-goal-tracker">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Base>
   );
 }
 
