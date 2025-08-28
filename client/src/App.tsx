c4902767d1ebbfd5f77baff8558f207cd35afa05
richardccf <richard@carboncopies.org>
2025-08-28 10:46:23 -0500

    revert deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Base } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,12 +8,10 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Base basePath="/cnwk-goal-tracker">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </Base>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
